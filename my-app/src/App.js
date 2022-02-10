e5747d0264fbc3fb2fc2bc58fea96c2581a1a94a
narayank97 <narayankarun@gmail.com>
2022-02-10 15:20:33 -0800

    completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,6 +15,14 @@ export const ACTIONS = {
 function reducer(state, {type,payload}){
 	switch(type){
 		case ACTIONS.ADD_DIGIT:
+			// after hitting = if you hit a digit the result will be replaced
+			if (state.overwrite){
+				return{
+					...state,
+					currentOperand:payload.digit,
+					overwrite:false
+				}
+			}
 			if(payload.digit === "0" && state.currentOperand === "0") return state
 			if(payload.digit === "." && state.currentOperand.includes(".")) return state
 
@@ -23,11 +31,104 @@ function reducer(state, {type,payload}){
 				currentOperand: `${state.currentOperand || ""}${payload.digit}`
 			}
 		case ACTIONS.CHOOSE_OPERATION:
-			if (state.currentOperand === null && state.prevOperand === null) return state
+			if (state.currentOperand == null && state.prevOperand == null){
+				return state
+			}
+			if (state.currentOperand == null){
+				return{
+					...state,
+					operation: payload.operation
+				}
+			} 
+			if (state.prevOperand == null){
+				return{
+					...state,
+					operation: payload.operation,
+					prevOperand: state.currentOperand,
+					currentOperand: null
+				}
+			}
+
+			console.log(state)
+			return{
+				...state,
+				prevOperand: evaluate(state),
+				operation: payload.operation,
+				currentOperand: null
+			}
+		case ACTIONS.EVALUATE:
+			if (
+				state.operation == null ||
+				state.currentOperand == null||
+				state.prevOperand == null
+			){
+				return state
+			}
+			return{
+				...state,
+				overwrite: true,
+				prevOperand: null,
+				operation:null,
+				currentOperand:evaluate(state)
+			}
+		case ACTIONS.DELETE_DIGIT:
+			if (state.overwrite){
+				return{
+					...state,
+					overwrite: false,
+					currentOperand: null
+				}
+			}
+			if (state.currentOperand == null){
+				return state
+			} 
+			if (state.currentOperand.length == 1){
+				return{
+					...state,
+					currentOperand:null
+				}
+			}
+			return{
+				...state,
+				currentOperand: state.currentOperand.slice(0,-1)
+			}
 		case ACTIONS.CLEAR:
 			return{}
 		
 	}
+}
+// this func takes in a state 
+function evaluate({currentOperand,prevOperand,operation}){
+	const prev = parseFloat(prevOperand)
+	const curr = parseFloat(currentOperand)
+	if (isNaN(prev) || isNaN(curr)) return ""
+	let computation = ""
+	switch(operation){
+		case "+":
+			computation = prev + curr
+			break
+		case "-":
+			computation = prev - curr
+			break
+		case "*":
+			computation = prev * curr
+			break
+		case "÷":
+			computation = prev / curr
+			break
+	}
+	return computation.toString()
+}
+
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us",{
+	maximumFractionDigits: 0,
+})
+
+function formatOperand(operand){
+	if (operand == null) return
+	const [integer,decimal] = operand.split(".")
+	if (decimal == null) return INTEGER_FORMATTER.format(integer)
+	return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
 }
 
 function App(){
@@ -36,11 +137,11 @@ function App(){
 	return (
 		<div className="calculator-grid">
 			<div className="output">
-				<div className="prev-operand">{prevOperand} {operation}</div>
-				<div className="current-operand">{currentOperand}</div>
+				<div className="prev-operand">{formatOperand(prevOperand)} {operation}</div>
+				<div className="current-operand">{formatOperand(currentOperand)}</div>
 			</div>
 			<button className="span-two" onClick={() => dispatch({type:ACTIONS.CLEAR})} >AC</button>
-			<button>DEL</button>
+			<button onClick= {() => dispatch({type:ACTIONS.DELETE_DIGIT})} >DEL</button>
 			<OperationButton operation="÷" dispatch={dispatch}/>
 			<DigitButton digit="1" dispatch={dispatch}/>
 			<DigitButton digit="2" dispatch={dispatch}/>
@@ -56,7 +157,7 @@ function App(){
 			<OperationButton operation="-" dispatch={dispatch}/>
 			<DigitButton digit="." dispatch={dispatch}/>
 			<DigitButton digit="0" dispatch={dispatch}/>
-			<button className="span-two">=</button>
+			<button className="span-two" onClick= {() => dispatch({type:ACTIONS.EVALUATE})}>=</button>
 		</div>
 	)
 }
